5a720d5b93cf1cd03acdbd0212bc27f8b8cfb0d4
Mike Gallaher <152364+pragsmike@users.noreply.github.com>
2025-06-10 01:29:06 -0400

    Add job stage.

--- src/tract/core.clj
+++ src/tract/core.clj
@@ -1,15 +1,14 @@
 (ns tract.core
   (:require [tract.pipeline :as pipeline]
+            [tract.stages.job :as job] ; <-- Add job stage
             [tract.stages.fetch :as fetch]
             [tract.stages.parser :as parser])
   (:gen-class))
 
-;; Define all stages of the pipeline in order.
 (def stages [:job :fetch :parser])
 
 (defn -main
-  "Main entry point for the entire `tract` application.
-  Initializes and runs all pipeline stages in sequence."
+  "Main entry point for the entire `tract` application."
   [& args]
   (println "--- Initializing Tract Pipeline ---")
   (doseq [stage-name stages]
@@ -17,12 +16,12 @@
     (pipeline/initialize-stage-dirs! stage-name))
   (println "--- Initialization Complete ---\n")
 
-  ;; Run each stage in order.
-  ;; In the future, we could add command-line args to run only specific stages.
+  ;; Run each stage in order
+  (job/run-stage!)
+  (println)
   (fetch/run-stage!)
-  (println) ; Add a blank line for readability
+  (println)
   (parser/run-stage!)
 
-  ;; Shut down the agent pool used by clj-http-lite and other async libs.
   (shutdown-agents)
   (println "\n--- Tract Pipeline Run Finished ---"))
